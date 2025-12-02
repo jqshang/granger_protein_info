6b49bc50b2651a2e443ec7dcfd0f61c6c866eecd
jqshang <charles.jq.shang@gmail.com>
2025-12-02 18:07:21 -0500

    try training on time difference

--- dataset/adk_data.py
+++ dataset/adk_data.py
@@ -49,4 +49,42 @@ def generate_adk_data():
         for label, info in label_to_residue.items()
     }
 
-    return positions, angles, amino_acids, amino_acids
+    return positions, angles, amino_acids
+
+
+def generate_adk_diff_data():
+
+    adk = datasets.fetch_adk_equilibrium()
+    u = mda.Universe(adk.topology, adk.trajectory)
+    ca = u.select_atoms("protein and name CA")
+
+    T = len(u.trajectory)
+    N = len(ca)
+    pos = np.zeros((T, N, 3))
+
+    for t, ts in enumerate(u.trajectory):
+        pos[t] = ca.positions.copy()
+
+    rama = Ramachandran(ca)
+    rama.run()
+    angles = rama.angles  # (T, 212, 2)
+
+    rama_residues = list(rama.ag3.residues)
+    rama_resids = np.array([res.resid for res in rama_residues])
+    ca_resids = np.array([res.resid for res in ca.residues])
+
+    resid_to_index = {resid: i for i, resid in enumerate(ca_resids)}
+    valid_residue_indices = np.array([resid_to_index[r] for r in rama_resids])
+
+    positions = pos[:, valid_residue_indices]  # (T, 212, 3)
+
+    num_residues = len(rama_residues)  # 212
+    amino_acids = [f"A{i+1}" for i in range(num_residues)]
+
+    # --- First differences ---
+    diff_positions = positions[1:] - positions[:-1]  # (T-1, 212, 3)
+
+    raw_diff_angles = angles[1:] - angles[:-1]  # (T-1, 212, 2)
+    diff_angles = (raw_diff_angles + 180.0) % 360.0 - 180.0
+
+    return diff_positions, diff_angles, amino_acids
